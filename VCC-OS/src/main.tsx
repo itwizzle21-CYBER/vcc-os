623458b78422b1d7d138e5b46d54f64a08e69d17
itwizzle21-CYBER <itwizzle21@gmail.com>
2026-06-19 04:20:13 +0000

    Restore VCC normal dev mode and regular functions

--- VCC-OS/src/main.tsx
+++ VCC-OS/src/main.tsx
@@ -2,16 +2,3 @@ import ReactDOM from "react-dom/client";
 import App from "../App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log("VCC_OS offline service worker active:", registration.scope);
-      })
-      .catch((error) => {
-        console.error("VCC_OS offline service worker failed:", error);
-      });
-  });
-}
